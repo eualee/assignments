199a22b0dce00daf4e1ebe61aba2586ce1ca20fc
eualee <eualee@iu.edu>
2026-09-13 21:44:44 -0400

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,366 @@
+#question 1
+tlist = [100.00, 80.00, 10.00, 50.00]
+clist = [36.57, 64.09, 3.81, 14.36]
+
+problem = 0 #a
+
+t = tlist[problem]
+c = clist[problem]
+
+tecents = int(t * 100 + 0.5)
+cocents = int(c * 100 + 0.5)
+chcents = tecents - cocents
+tocents = chcents
+
+hundreds = chcents // 10000
+chcents = chcents % 10000
+
+fifties = chcents // 5000
+chcents = chcents % 5000
+
+twenties = chcents // 2000
+chcents = chcents % 2000
+
+tens = chcents // 1000
+chcents = chcents % 1000
+
+fives = chcents // 500
+chcents = chcents % 500
+
+ones = chcents // 100
+chcents = chcents % 100
+
+quarter = chcents // 25
+chcents = chcents % 25
+
+dime = chcents // 10
+chcents = chcents % 10
+
+nickel = chcents // 5
+chcents = chcents % 5
+
+pennies = chcents
+
+tetext = "$" + str(tecents // 100) + "." + str(tecents % 100 + 100)[1:]
+cotext = "$" + str(cocents // 100) + "." + str(cocents % 100 + 100)[1:]
+
+print("Amount tendered:" + ' ' + tetext)
+print("Total cost:" + ' ' + cotext)
+print("$100 bills:" + ' ' + str(hundreds))
+print("$50 bills:" + ' ' + str(fifties))
+print("$20 bills:" + ' ' + str(twenties))
+print("$10 bills:" + ' ' + str(tens))
+print("$5 bills:" + ' ' + str(fives))
+print("$1 bills:" + ' ' + str(ones))
+print("Quarters:" + ' ' + str(quarter))
+print("Dimes:" + ' ' + str(dime))
+print("Nickels:" + ' ' + str(nickel))
+print("Pennies:" + ' ' + str(pennies))
+
+tlist = [100.00, 80.00, 10.00, 50.00]
+clist = [36.57, 64.09, 3.81, 14.36]
+
+problem = 1 #b
+
+t = tlist[problem]
+c = clist[problem]
+
+tecents = int(t * 100 + 0.5)
+cocents = int(c * 100 + 0.5)
+chcents = tecents - cocents
+tocents = chcents
+
+hundreds = chcents // 10000
+chcents = chcents % 10000
+
+fifties = chcents // 5000
+chcents = chcents % 5000
+
+twenties = chcents // 2000
+chcents = chcents % 2000
+
+tens = chcents // 1000
+chcents = chcents % 1000
+
+fives = chcents // 500
+chcents = chcents % 500
+
+ones = chcents // 100
+chcents = chcents % 100
+
+quarter = chcents // 25
+chcents = chcents % 25
+
+dime = chcents // 10
+chcents = chcents % 10
+
+nickel = chcents // 5
+chcents = chcents % 5
+
+pennies = chcents
+
+tetext = "$" + str(tecents // 100) + "." + str(tecents % 100 + 100)[1:]
+cotext = "$" + str(cocents // 100) + "." + str(cocents % 100 + 100)[1:]
+
+print("Amount tendered:" + ' ' + tetext)
+print("Total cost:" + ' ' + cotext)
+print("$100 bills:" + ' ' + str(hundreds))
+print("$50 bills:" + ' ' + str(fifties))
+print("$20 bills:" + ' ' + str(twenties))
+print("$10 bills:" + ' ' + str(tens))
+print("$5 bills:" + ' ' + str(fives))
+print("$1 bills:" + ' ' + str(ones))
+print("Quarters:" + ' ' + str(quarter))
+print("Dimes:" + ' ' + str(dime))
+print("Nickels:" + ' ' + str(nickel))
+print("Pennies:" + ' ' + str(pennies))
+
+tlist = [100.00, 80.00, 10.00, 50.00]
+clist = [36.57, 64.09, 3.81, 14.36]
+
+problem = 2 #c
+
+t = tlist[problem]
+c = clist[problem]
+
+tecents = int(t * 100 + 0.5)
+cocents = int(c * 100 + 0.5)
+chcents = tecents - cocents
+tocents = chcents
+
+hundreds = chcents // 10000
+chcents = chcents % 10000
+
+fifties = chcents // 5000
+chcents = chcents % 5000
+
+twenties = chcents // 2000
+chcents = chcents % 2000
+
+tens = chcents // 1000
+chcents = chcents % 1000
+
+fives = chcents // 500
+chcents = chcents % 500
+
+ones = chcents // 100
+chcents = chcents % 100
+
+quarter = chcents // 25
+chcents = chcents % 25
+
+dime = chcents // 10
+chcents = chcents % 10
+
+nickel = chcents // 5
+chcents = chcents % 5
+
+pennies = chcents
+
+tetext = "$" + str(tecents // 100) + "." + str(tecents % 100 + 100)[1:]
+cotext = "$" + str(cocents // 100) + "." + str(cocents % 100 + 100)[1:]
+
+print("Amount tendered:" + ' ' + tetext)
+print("Total cost:" + ' ' + cotext)
+print("$100 bills:" + ' ' + str(hundreds))
+print("$50 bills:" + ' ' + str(fifties))
+print("$20 bills:" + ' ' + str(twenties))
+print("$10 bills:" + ' ' + str(tens))
+print("$5 bills:" + ' ' + str(fives))
+print("$1 bills:" + ' ' + str(ones))
+print("Quarters:" + ' ' + str(quarter))
+print("Dimes:" + ' ' + str(dime))
+print("Nickels:" + ' ' + str(nickel))
+print("Pennies:" + ' ' + str(pennies))
+
+tlist = [100.00, 80.00, 10.00, 50.00]
+clist = [36.57, 64.09, 3.81, 14.36]
+
+problem = 3 #d
+
+t = tlist[problem]
+c = clist[problem]
+
+tecents = int(t * 100 + 0.5)
+cocents = int(c * 100 + 0.5)
+chcents = tecents - cocents
+tocents = chcents
+
+hundreds = chcents // 10000
+chcents = chcents % 10000
+
+fifties = chcents // 5000
+chcents = chcents % 5000
+
+twenties = chcents // 2000
+chcents = chcents % 2000
+
+tens = chcents // 1000
+chcents = chcents % 1000
+
+fives = chcents // 500
+chcents = chcents % 500
+
+ones = chcents // 100
+chcents = chcents % 100
+
+quarter = chcents // 25
+chcents = chcents % 25
+
+dime = chcents // 10
+chcents = chcents % 10
+
+nickel = chcents // 5
+chcents = chcents % 5
+
+pennies = chcents
+
+tetext = "$" + str(tecents // 100) + "." + str(tecents % 100 + 100)[1:]
+cotext = "$" + str(cocents // 100) + "." + str(cocents % 100 + 100)[1:]
+
+print("Amount tendered:" + ' ' + tetext)
+print("Total cost:" + ' ' + cotext)
+print("$100 bills:" + ' ' + str(hundreds))
+print("$50 bills:" + ' ' + str(fifties))
+print("$20 bills:" + ' ' + str(twenties))
+print("$10 bills:" + ' ' + str(tens))
+print("$5 bills:" + ' ' + str(fives))
+print("$1 bills:" + ' ' + str(ones))
+print("Quarters:" + ' ' + str(quarter))
+print("Dimes:" + ' ' + str(dime))
+print("Nickels:" + ' ' + str(nickel))
+print("Pennies:" + ' ' + str(pennies))
+
+#Question 2
+
+listnamefirst = ["Linus", "Alan", "Stephan", "Adele"]
+listnamemiddle = ["Benedict", "Mathison", "Gary", ""]
+listnamelast = ["Torvalds", "Turing", "Wozniak", "Goldberg"]
+listnumber = ["(317)-555-1969", "(317)-555-5346", "(317)-555-3720", "(317)-555-2345"]
+
+person = 0 #a
+
+first = listnamefirst[person]
+middle = listnamemiddle[person]
+last = listnamelast[person]
+phone = listnumber[person]
+
+lastname = last[0:6]
+firstinitial = first[0:1]
+middleinitial = middle[0:1]
+
+username = lastname + firstinitial + middleinitial
+
+firsttwo = first[0:2]
+evenletters = middle[1:6:2]
+lastfour = phone[-4:]
+
+password = firsttwo + evenletters + lastfour
+
+print(" ")
+print("First name:" + " " + first)
+print("Middle name:" + " " + middle)
+print("Last name:" + " " + last)
+print("Phone number:" + " " + phone)
+print(" ")
+print("Username:" + " " + username)
+print("Password:" + " " + password)
+
+listnamefirst = ["Linus", "Alan", "Stephan", "Adele"]
+listnamemiddle = ["Benedict", "Mathison", "Gary", ""]
+listnamelast = ["Torvalds", "Turing", "Wozniak", "Goldberg"]
+listnumber = ["(317)-555-1969", "(317)-555-5346", "(317)-555-3720", "(317)-555-2345"]
+
+person = 1 #b
+
+first = listnamefirst[person]
+middle = listnamemiddle[person]
+last = listnamelast[person]
+phone = listnumber[person]
+
+lastname = last[0:6]
+firstinitial = first[0:1]
+middleinitial = middle[0:1]
+
+username = lastname + firstinitial + middleinitial
+
+firsttwo = first[0:2]
+evenletters = middle[1:6:2]
+lastfour = phone[-4:]
+
+password = firsttwo + evenletters + lastfour
+
+print(" ")
+print("First name:" + " " + first)
+print("Middle name:" + " " + middle)
+print("Last name:" + " " + last)
+print("Phone number:" + " " + phone)
+print(" ")
+print("Username:" + " " + username)
+print("Password:" + " " + password)
+
+listnamefirst = ["Linus", "Alan", "Stephan", "Adele"]
+listnamemiddle = ["Benedict", "Mathison", "Gary", ""]
+listnamelast = ["Torvalds", "Turing", "Wozniak", "Goldberg"]
+listnumber = ["(317)-555-1969", "(317)-555-5346", "(317)-555-3720", "(317)-555-2345"]
+
+person = 2 #c
+
+first = listnamefirst[person]
+middle = listnamemiddle[person]
+last = listnamelast[person]
+phone = listnumber[person]
+
+lastname = last[0:6]
+firstinitial = first[0:1]
+middleinitial = middle[0:1]
+
+username = lastname + firstinitial + middleinitial
+
+firsttwo = first[0:2]
+evenletters = middle[1:6:2]
+lastfour = phone[-4:]
+
+password = firsttwo + evenletters + lastfour
+
+print(" ")
+print("First name:" + " " + first)
+print("Middle name:" + " " + middle)
+print("Last name:" + " " + last)
+print("Phone number:" + " " + phone)
+print(" ")
+print("Username:" + " " + username)
+print("Password:" + " " + password)
+
+listnamefirst = ["Linus", "Alan", "Stephan", "Adele"]
+listnamemiddle = ["Benedict", "Mathison", "Gary", ""]
+listnamelast = ["Torvalds", "Turing", "Wozniak", "Goldberg"]
+listnumber = ["(317)-555-1969", "(317)-555-5346", "(317)-555-3720", "(317)-555-2345"]
+
+person = 3 #d
+
+first = listnamefirst[person]
+middle = listnamemiddle[person]
+last = listnamelast[person]
+phone = listnumber[person]
+
+lastname = last[0:6]
+firstinitial = first[0:1]
+middleinitial = middle[0:1]
+
+username = lastname + firstinitial + middleinitial
+
+firsttwo = first[0:2]
+evenletters = middle[1:6:2]
+lastfour = phone[-4:]
+
+password = firsttwo + evenletters + lastfour
+
+print(" ")
+print("First name:" + " " + first)
+print("Middle name:" + " " + middle)
+print("Last name:" + " " + last)
+print("Phone number:" + " " + phone)
+print(" ")
+print("Username:" + " " + username)
+print("Password:" + " " + password)
